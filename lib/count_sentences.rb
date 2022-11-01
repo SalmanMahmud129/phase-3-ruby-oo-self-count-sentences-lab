a42cb2f366b968081fa4f7d1f12675dcd8d28fa7
Salman Mahmud <salman_mahmud@hotmail.com>
2022-11-01 19:48:35 -0400

    commit tree 34d458868d687cd842cf499b109422e0be989a35

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+    # binding.pry
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
   end
 end
+
+"The weather outside is frightful.".sentence?
+"But the fire is so delightful".sentence?
